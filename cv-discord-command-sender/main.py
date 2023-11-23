8f90fd9f3956fb755ff1295ea0dfefd0a0fd621e
blurry16 <147284253+blurry16@users.noreply.github.com>
2023-11-23 15:31:17 +0300

    Update main.py
    Added /getuuid and Mojang API fixed

--- cv-discord-command-sender/main.py
+++ cv-discord-command-sender/main.py
@@ -6,6 +6,7 @@ from disnake.ext import commands
 bot = commands.Bot(command_prefix='$', intents=disnake.Intents.all())
 mojang_api = API()
 
+
 # Admins list. (Discord nicknames)
 admins = ['blurry16', 'itsmefred']
 
@@ -31,7 +32,7 @@ def mcprint(text):
 @bot.event
 async def on_ready():
     print(f'Bot {bot.user} is ready to work.')
-    await bot.change_presence(status=disnake.Status.online)
+    await bot.change_presence(status=disnake.Status.online, activity=disnake.Game('on cubeville.org'))
 
 
 # Message logging in terminal
@@ -47,6 +48,27 @@ async def on_message(message):
     print(f'{author} said: {user_message} in ({channel}).')
 
 
+# Help command
+@bot.slash_command(description=f'Help for new admins')
+async def helpme(ctx):
+    user = str(ctx.user)
+    for admin in admins:
+        if user == admin:
+            print(f"{user} executed /helpme slash command.")
+            await ctx.send(f'`List of commands:\n'
+                           f'/helpme - this command :p\n'
+                           f'/addmember - the same as /rg addmember blurry16 <nickname>. Just put the nickname and the player will be added\n'
+                           f'/removemember - the same as /rg removemember blurry16 <nickname>. Just put the nickname and the player will be removed.\n'
+                           f'/rgflag - the same as /rg flag blurry16 <flag_name> <flag_option>. <flag_option> can be not only ALLOW/DENY! It is better to check flags in game before executing this command.\n'
+                           f'/chat - local chat using\n'
+                           f'/uuid - get uuid of someone with their mojang nickname\n'
+                           f'The bot will not be on 24/7. It is online only when blurry16 is afk or sleeping.`', ephemeral=True)
+            break
+    else:
+        await ctx.send(f'No permissions.', ephemeral=True)
+        print(f'{user} used /helpme (NO PERMISSIONS)')
+
+
 # /addmember command | /rg addmember {region_name} {nickname}
 @bot.slash_command(description=f'# /rg addmember {region_name} [nickname] INGAME')
 async def addmember(ctx, nickname):
@@ -57,6 +79,8 @@ async def addmember(ctx, nickname):
             try:
                 uuid = str(mojang_api.get_uuid(nickname))
                 if uuid is not False:
+                    profile = mojang_api.get_profile(uuid)
+                    nickname = str(profile.name)
                     print(f"{user} executed addmember {nickname} ({uuid}) slash command.")
                     mcprint(f'/rg addmember {region_name} {nickname}')
                     if msg_recipient is not None:
@@ -82,6 +106,8 @@ async def removemember(ctx, nickname):
             try:
                 uuid = str(mojang_api.get_uuid(nickname))
                 if uuid is not False:
+                    profile = mojang_api.get_profile(uuid)
+                    nickname = str(profile.name)
                     print(f"{user} executed removemember {nickname} ({uuid}) slash command.")
                     mcprint(f'/rg removemember {region_name} {nickname}')
                     if msg_recipient is not None:
@@ -115,26 +141,7 @@ async def flag(ctx, flag_name, flag_option):
         print(f'{user} executed flag {flag_name} {flag_option} slash command. (NO PERMISSIONS)')
 
 
-# Help command
-@bot.slash_command(description=f'Help for new admins')
-async def helpme(ctx):
-    user = str(ctx.user)
-    for admin in admins:
-        if user == admin:
-            print(f"{user} executed /helpme slash command.")
-            await ctx.send(f'`List of commands:\n'
-                           f'/helpme - this command :p\n'
-                           f'/addmember - the same as /rg addmember blurry16 <nickname>. Just put the nickname and the player will be added\n'
-                           f'/removemember - the same as /rg removemember blurry16 <nickname>. Just put the nickname and the player will be removed.\n'
-                           f'/rgflag - the same as /rg flag blurry16 <flag_name> <flag_option>. <flag_option> can be not only ALLOW/DENY! It is better to check flags in game before executing this command.\n'
-                           f'/chat - local chat using'
-                           f'The bot will not be on 24/7. It is online only when blurry16 is afk or sleeping.`', ephemeral=True)
-            break
-    else:
-        await ctx.send(f'No permissions.', ephemeral=True)
-        print(f'{user} used /helpme (NO PERMISSIONS)')
-
-
+# Local chat command
 @bot.slash_command(description=f'local chat')
 async def chat(ctx, message):
     user = str(ctx.user)
@@ -148,6 +155,31 @@ async def chat(ctx, message):
     else:
         await ctx.send(f'No permissions.', ephemeral=True)
         print(f'{user} used /chat {message} (NO PERMISSIONS)')
+
+
+# Get UUID of a Mojang account command
+@bot.slash_command(description=f'Mojang account UUID getter')
+async def getuuid(ctx, nickname):
+    user = str(ctx.user)
+    for admin in admins:
+        if user == admin:
+            print(f'{user} executed /getuuid {nickname} command')
+            try:
+                uuid = str(mojang_api.get_uuid(nickname))
+                if uuid is not False:
+                    profile = mojang_api.get_profile(uuid)
+                    nickname = str(profile.name)
+                    await ctx.send(f'{nickname}\'s UUID is - {uuid}')
+                    print(f'{user} got UUID of {nickname} ({uuid})')
+                break
+            except:
+                await ctx.send(f'This profile does not exist. ({nickname})')
+                print(f'{user} did not get UUID of {nickname}. THIS PROFILE DOES NOT EXIST')
+                break
+
+    else:
+        await ctx.send(f'No permissions.', ephemeral=True)
+        print(f'{user} used /getuuid {nickname} (NO PERMISSIONS)')
 
 
 # Bot run
